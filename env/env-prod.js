a2c260fdd2a51e58ff464802dffcd150a3e3d3bc
luisrowley <louisdebenito@gmail.com>
2023-04-28 19:02:54 +0200

    workflow for fetching public key from remote server.

--- env/env-prod.js
+++ env/env-prod.js
@@ -1,5 +1,5 @@
 const ENV_VARS = {
-    REMOTE_PUBLIC_KEY_ENDPOINT_URL: 'http://example.com/getpublickey'
+    REMOTE_PUBLIC_KEY_ENDPOINT_URL: 'http://localhost:3000/getkey'
 }
 
 module.exports = ENV_VARS;
